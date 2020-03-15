7ed4ace9b3b29ae15ba0e856c1a63ee4455f2a4e
Osamu Nagano <onagano@redhat.com>
2020-03-15 20:05:36 +0900

    add logging helper functions

--- helper.sh
+++ helper.sh
@@ -79,3 +79,74 @@ function deploy-web() {
 }
 
 
+# Enable and disable HTTP access log
+
+# CMD_1 はroload（再起動）が必要で、初回のみでいい。
+ENABLE_ACCESS_LOG_CMD_1='/subsystem=undertow/server=default-server/http-listener=default:write-attribute(name=record-request-start-time,value=true)'
+ENABLE_ACCESS_LOG_CMD_2='/subsystem=undertow/server=default-server/host=default-host/setting=access-log:add(pattern="%h %l %u %t \"%r\" %s %b \"%{i,Referer}\" \"%{i,User-Agent}\" Cookie: \"%{i,COOKIE}\" Set-Cookie: \"%{o,SET-COOKIE}\" SessionID: %S Thread: \"%I\" TimeTaken: %T")'
+DISABLE_ACCESS_LOG_CMD='/subsystem=undertow/server=default-server/host=default-host/setting=access-log:remove'
+
+fundtion sso-enable-access-log() {
+    #sso-cli "$ENABLE_ACCESS_LOG_CMD_1"
+    sso-cli "$ENABLE_ACCESS_LOG_CMD_2"
+}
+
+fundtion sso-disable-access-log() {
+    sso-cli "$DISABLE_ACCESS_LOG_CMD"
+}
+
+fundtion api-enable-access-log() {
+    #api-cli "$ENABLE_ACCESS_LOG_CMD_1"
+    api-cli "$ENABLE_ACCESS_LOG_CMD_2"
+}
+
+fundtion api-disable-access-log() {
+    api-cli "$DISABLE_ACCESS_LOG_CMD"
+}
+
+fundtion web-enable-access-log() {
+    #web-cli "$ENABLE_ACCESS_LOG_CMD_1"
+    web-cli "$ENABLE_ACCESS_LOG_CMD_2"
+}
+
+fundtion web-disable-access-log() {
+    web-cli "$DISABLE_ACCESS_LOG_CMD"
+}
+
+
+# Enable and disable request dumper
+
+ENABLE_REQUEST_DUMPER_CMD_1='/subsystem=undertow/configuration=filter/expression-filter=requestDumperExpression:add(expression="dump-request")'
+ENABLE_REQUEST_DUMPER_CMD_2='/subsystem=undertow/server=default-server/host=default-host/filter-ref=requestDumperExpression:add'
+DISABLE_REQUEST_DUMPER_CMD_1='/subsystem=undertow/server=default-server/host=default-host/filter-ref=requestDumperExpression:remove'
+DISABLE_REQUEST_DUMPER_CMD_2='/subsystem=undertow/configuration=filter/expression-filter=requestDumperExpression:remove'
+
+fundtion sso-enable-request-dumper() {
+    sso-cli "$ENABLE_REQUEST_DUMPER_CMD_1"
+    sso-cli "$ENABLE_REQUEST_DUMPER_CMD_2"
+}
+
+fundtion sso-disable-request-dumper() {
+    sso-cli "$DISABLE_REQUEST_DUMPER_CMD_1"
+    sso-cli "$DISABLE_REQUEST_DUMPER_CMD_2"
+}
+
+fundtion api-enable-request-dumper() {
+    api-cli "$ENABLE_REQUEST_DUMPER_CMD_1"
+    api-cli "$ENABLE_REQUEST_DUMPER_CMD_2"
+}
+
+fundtion api-disable-request-dumper() {
+    api-cli "$DISABLE_REQUEST_DUMPER_CMD_1"
+    api-cli "$DISABLE_REQUEST_DUMPER_CMD_2"
+}
+
+fundtion web-enable-request-dumper() {
+    web-cli "$ENABLE_REQUEST_DUMPER_CMD_1"
+    web-cli "$ENABLE_REQUEST_DUMPER_CMD_2"
+}
+
+fundtion web-disable-request-dumper() {
+    web-cli "$DISABLE_REQUEST_DUMPER_CMD_1"
+    web-cli "$DISABLE_REQUEST_DUMPER_CMD_2"
+}
